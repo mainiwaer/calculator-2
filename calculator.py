2541de14f85b44ddfdc3cb38e4e2198385e3ffe1
Tina Haiser <tinahaiser@gmail.com>
2021-02-25 00:34:53 +0000

    Wrote pseudocode, made intro statement, started while loop, made break statement
    Co-authored-by: Munawaer Ainiwaer <munawaer.ainiwaer@gmail.com>

--- calculator.py
+++ calculator.py
@@ -4,4 +4,20 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# Write while loop that asks for input of numbers and the operator 
+# use split function to turn the string input into a list
+# Assign first item in list to operator variable, the rest to num1, num2, etc
+# For each operation type, we need to write a conditional that evaluated for the operator
+# calls function from arithmetic file
+
+print("Welcome to The Calculator (part 2)")
+
+while True:
+
+    user_equation = input("Enter your equation > ")
+
+    if user_equation == "q" or user_equation == "quit":
+        print("I guess I'll calc-u-later! :^)")
+        break
+    
+    
